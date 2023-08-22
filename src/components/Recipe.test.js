7d93cec4f0649f0122c590811c0cc91711a0a936
Denys <go4denys@gmail.com>
2023-08-22 10:35:56 -0400

    Rendering the component for tests

---
+++ src/components/Recipe.test.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import '@testing-library/jest-dom'
+import { render, screen } from '@testing-library/react'
+import Recipe from './Results/Recipe'
+
+test("render content", () => {
+    const recipe = {
+    name: "Chicken Caesar Salad",
+      ingredients: [
+        "2 chicken breasts, grilled and sliced",
+        "1 head romaine lettuce, washed and torn",
+        "1/2 cup Caesar dressing",
+        "1/2 cup grated Parmesan cheese",
+        "1 cup croutons"
+      ],
+      instructions: [
+        "Arrange the lettuce on a large platter.",
+        "Place the grilled chicken slices on top of the lettuce.",
+        "Drizzle the Caesar dressing over the salad.",
+        "Sprinkle the Parmesan cheese and croutons over the top.",
+        "Serve immediately."
+      ],
+      category: "Salad",
+      image: "https://www.jessicagavin.com/wp-content/uploads/2022/06/chicken-caesar-salad-24-600x900.jpg",
+      like: true
+    }
+
+    render(<Recipe recipe={recipe} />)
+
+    const element = screen.getByText("Chicken Caesar Salad")
+    expect(element).toBeDefined()
+})
